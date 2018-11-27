c92ef0c3b0dfe21e57ee869fc1b828a68aab6362
kashifch <kashif.chaudhry@arbisoft.com>
2018-11-27 14:51:56 +0500

    better login mechanism

--- cypress/integration/existing_user/journal_operations.js
+++ cypress/integration/existing_user/journal_operations.js
@@ -1,14 +1,19 @@
 import {
   expandTocItem,
+  verifyTocItemName,
   verifyPageTitle,
   goToPage,
 } from '../../support/utils'
 
 describe('Verify Journal Navigation', () => {
   // Login to edX stage using request and get user session
-  beforeEach(() => {
+  before(() => {
     cy.login_using_api(Cypress.env('JOURNAL_USER_EMAIL'), Cypress.env('JOURNAL_USER_PASSWORD'))
+  })
+
+  beforeEach(() => {
     // Use the above user session to login to Journals
+    Cypress.Cookies.preserveOnce('edxloggedin', 'stage-edx-user-info', 'stage-edx-sessionid')
     cy.visit('/')
     cy.get('.header-actions > .btn').contains('Login').click()
   })
@@ -30,25 +35,19 @@ describe('Verify Journal Navigation', () => {
     // Check the presence of First Main Item(Chapter)
     cy.get('.toc>ul>li').first().as('firstMainItem')
     // Check Chapter Name
-    cy.get('@firstMainItem')
-      .find('a').first()
-      .should('have.text', 'Chapter 1')
+    verifyTocItemName('@firstMainItem', 'Chapter 1')
     // Check that by default chapter is not expanded but expands on click
     expandTocItem('@firstMainItem')
     // Check the presence of First Main Item's child
     cy.get('@firstMainItem').find('ul').first().as('firstChildItem')
     // Check Child Name
-    cy.get('@firstChildItem')
-      .find('a').first()
-      .should('have.text', '1.1')
+    verifyTocItemName('@firstChildItem', '1.1')
     // Check that by default child is not expanded but expands on click
     expandTocItem('@firstChildItem')
     // Check the presence of First Main Item's grandchild
     cy.get('@firstChildItem').find('ul').first().as('firstGrandChildItem')
     // Check GrandChild Name
-    cy.get('@firstGrandChildItem')
-      .find('a').first()
-      .should('have.text', '1.1.1')
+    verifyTocItemName('@firstGrandChildItem', '1.1.1')
   })
 
   it('browse pages using next and previous button', () => {
@@ -68,58 +67,3 @@ describe('Verify Journal Navigation', () => {
   })
 })
 
-describe('Verify Journal Pages', () => {
-  // Login to edX stage using request and get user session
-  beforeEach(() => {
-    cy.visit('/')
-    cy.login_from_ui(Cypress.env('JOURNAL_USER_EMAIL'), Cypress.env('JOURNAL_USER_PASSWORD'))
-  })
-
-  it('checks Text page', () => {
-    const bodyTexts = ['Simple text', 'Raw html text.']
-    // Click on the Journal card
-    cy.contains('E2E Tests Journal').click()
-    // Go to a specific page in a specific chapter
-    goToPage('Chapter 2', 'Text')
-    cy.get('.journal-page-body')
-      .find('.body-element p').each(($bodyElement, index) => {
-        cy.wrap($bodyElement).should('have.text', bodyTexts[index])
-      })
-  })
-
-  it('checks Images page', () => {
-    const altTexts = ['Apple Sauce', 'Quote']
-    // Click on the Journal card
-    cy.contains('E2E Tests Journal').click()
-    // Go to a specific page in a specific chapter
-    goToPage('Chapter 2', 'Images')
-    cy.get('.journal-page-body')
-      .find('.body-element img').each(($bodyElement, index) => {
-        cy.wrap($bodyElement).should('have.attr', 'alt', altTexts[index])
-      })
-  })
-
-  it('checks Documents page', () => {
-    const docIds = ['pdf-70efdf2ec9b086079795c442636b55fb', 'pdf-a87ff679a2f3e71d9181a67b7542122c']
-    // Click on the Journal card
-    cy.contains('E2E Tests Journal').click()
-    // Go to a specific page in a specific chapter
-    goToPage('Chapter 2', 'Documents')
-    cy.get('.journal-page-body')
-      .find('.body-element>span').each(($bodyElement, index) => {
-        cy.wrap($bodyElement).should('have.attr', 'id', docIds[index])
-      })
-  })
-
-  it('checks Video page', () => {
-    const vidIds = ['xblock_video-e2a2dcc36a08a345332c751b2f2e476c', 'xblock_video-31839b036f63806cba3f47b93af8ccb5']
-    // Click on the Journal card
-    cy.contains('E2E Tests Journal').click()
-    // Go to a specific page in a specific chapter
-    goToPage('Chapter 2', 'Videos')
-    cy.get('.journal-page-body')
-      .find('.body-element>span').each(($bodyElement, index) => {
-        cy.wrap($bodyElement).should('have.attr', 'id', vidIds[index])
-      })
-  })
-})
